5fb57be7091424054e947cde026d16644dd8d972
Apurba <palapurba2004@gmail.com>
2025-02-19 11:50:39 +0530

    working

--- scraping.py
+++ scraping.py
@@ -2,8 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from openpyxl import Workbook
 
-# URL of the website (replace with the actual URL)
-URL = "https://www.aucse.in/people/student/btech/cse-batch-2022-2026"  # Replace with the actual URL
+URL = "https://www.aucse.in/people/student/btech/cse-batch-2022-2026"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
 }
@@ -16,10 +15,10 @@ soup = BeautifulSoup(response.text, "html.parser")
 wb = Workbook()
 sheet = wb.active
 sheet.title = "Scraped Data"
-sheet.append(["Roll Number", "Name", "Image URL", "Department"])
+sheet.append(["Sl No. ", "Roll No.", "Image URL", "Name"])
 
 # Extracting data from HTML
-sections = soup.find_all("section", {"class": ["yaqOZd cJgDec tpmmCb", "yaqOZd"]})
+sections = soup.find_all("section", {"class": ["yaqOZd","cJgDec","tpmmCb"]})
 for section in sections:
     paragraphs = section.find_all("p", class_="zfr3Q CDt4Ke")
     roll_number = paragraphs[0].text.strip() if len(paragraphs) > 0 else "N/A"
